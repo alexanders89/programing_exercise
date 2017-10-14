8ff447d5a560860804873b865cb6b7bc0b105623
Alex <alex.satur89@gmail.com>
2017-10-14 19:16:47 +0100

    started building interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -4,14 +4,14 @@ def interactive_menu
   loop do
     puts "===================="
     puts "1. Check Individual URL"
-    puts "2. View URL"
+    puts "2. Check Multiple URL"
     puts "9. Exit"
     selection = gets.chomp
     case selection
     when "1"
       get_url
     when "2"
-      view_url
+      check_multi_url
     when "9"
       exit
     end
@@ -20,14 +20,22 @@ end
 
 def get_url
   puts "Enter URL:"
-  @report = Report.new(gets.chomp)
+  report = Report.new(gets.chomp)
+  report.print_output
+  puts report.output
 end
 
-def view_url
-  puts @report.url
-end
-
-def request_url
+def check_multi_url
+    puts 'Please enter the number dish you would like to order'
+    index = gets.chomp
+    while !index.empty? do
+      puts 'Please enter the quantity'
+      quantity = gets.chomp.to_i
+      puts "#{quantity} x #{@students[index.to_i - 1][:name]} have been added to your basket"
+      @basket << [@students[(index.to_i) -1][:name], quantity, @students[(index.to_i) - 1][:price]]
+      puts 'Please enter the number dish you would like to order'
+      index = gets.chomp
+    end
 
 end
 
