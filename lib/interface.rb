ee60de3e7911efdee527626805c56f3444c1fc47
Alex <alex.satur89@gmail.com>
2017-10-15 15:39:26 +0100

    finished itnerface, table building after the list of urls has run

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,10 +1,13 @@
 require './report'
 require './multireport'
-
+require 'terminal-table'
+require 'colorize'
 
 @list = []
+@table = []
 
 def interactive_menu
+  puts "\e[H\e[2J"
   loop do
     puts "===================="
     puts "1. Check Individual URL"
@@ -26,6 +29,7 @@ def interactive_menu
 end
 
 def get_url
+  puts "\e[H\e[2J"
   puts "Enter URL:"
   report = Report.new(gets.chomp)
   report.print_output
@@ -33,8 +37,9 @@ def get_url
 end
 
 def check_multi_url
-    puts "Enter URL, to finish press return twice."
-    url = gets.chomp
+  puts "\e[H\e[2J"
+  puts "Enter URL, to finish press return twice."
+  url = gets.chomp
     while !url.empty? do
       puts "#{url} has been added"
       @list << url
@@ -51,19 +56,47 @@ end
 def run_urls
   @list.each do |url|
     report = Report.new(url)
+    @table << [report.url, report.get_code, report.get_content_length, report.get_date]
     report.print_output
     puts report.output
   end
+  build_table
+  @table = []
   @list = []
 end
 
 def load_list
-  file = File.open("sample_data.txt", "r")
+  puts 'Choose a list to open'
+  files = Dir.entries('../sample_data')
+  files.each {|file| puts file[0, file.index('.')]}
+  file_to_load = gets.chomp
+  file = File.open("../sample_data/#{file_to_load}.txt", "r")
   file.readlines.each do |line|
     @list << line.chop
   end
   file.close
   run_urls
 end
+
+def build_table
+  rows = []
+  @table.each { |url, code, length, date| rows << [
+    url, code, length, date] }
+    table = Terminal::Table.new :headings => ['Url'.red, 'Code'.red, 'Content Length'.red, 'Date'.red],
+    :rows => rows
+    puts table
+end
+
+def view_basket
+  header
+  rows = []
+  @basket.each { |dish, quantity, price| rows << [quantity,
+  dish, price, price.to_i * quantity.to_i ] }
+  table = Terminal::Table.new :headings => ['Qty'.red, 'Dish'.red, 'Price'.red, 'Total'.red],
+  :rows => rows
+  puts table
+  sum_basket
+end
+
 
 interactive_menu
